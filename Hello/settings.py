f926c2e095f1203340ae21ae125e00467705a4f1
faisal7008 <faisalhussain7008@gmail.com>
2022-06-23 22:42:44 +0530

    commit

--- Hello/settings.py
+++ Hello/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'Hello.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "templates"],       # Added Manually
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
