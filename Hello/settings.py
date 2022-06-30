898cd4c752a94c2c009dbc9e27af00f8673919ec
faisal7008 <faisalhussain7008@gmail.com>
2022-06-30 22:12:28 +0530

    added database connection

--- Hello/settings.py
+++ Hello/settings.py
@@ -95,6 +95,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 WHITENOISE_USE_FINDERS = True       # Added manually
 
